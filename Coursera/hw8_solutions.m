a489d13e194133285612176c190afe618dd27609
srishtis <srishti280992@yahoo.com>
2018-08-25 19:17:29 +0530

    updated with questions

--- Coursera/hw8_solutions.m
+++ Coursera/hw8_solutions.m
@@ -2,6 +2,11 @@ digit_counter('huge_add.m');
 a=day_counter( 1988)
 circular_primes( 8226 );
 
+%Q1: Write a function called digit_counter that takes the name of a text file as input and returns the
+%number of digits (i.e., any of the characters, 0-to-9) that the file contains. If there is a problem opening the
+%file, the function returns -1. WARNING: if you use the ‘w’ flag with fopen, as opposed to ‘r’, you will
+%overwrite the file. The grader uses your own m files to check your function, so be careful!
+
 %function: digit_counter in a file; takes file name as argument
 function dc = digit_counter(filename)
     dc=-1;
@@ -11,6 +16,12 @@ function dc = digit_counter(filename)
         fclose(fid);
     end
 end
+
+%Q2: Write a function called day_counter that returns the number of Mondays that fell on the first day of the
+%month in a given year between 1776 and 2016 inclusive where the requested year is the only input to your
+%function and it is a positive integer scalar. Note that a leap year occurs on any year evenly divisible by 4, but
+%not on a century unless it is divisible by 400. In a leap year, February has 29 days. You are not allowed to use
+%the datenum built-in function. (Inspired by Project Euler.)
 
 % function to count number of mondays
 function foms = day_counter( year )
@@ -25,6 +36,166 @@ function foms = day_counter( year )
    foms=sum(weekday(datetime(year,1:12,1))==2);
 end
 
+%Q3: Write function called huge_add that adds together two positive integers of any length specified as char
+%vectors using decimal notation. The single output argument is the result and it is a char vector as well. The
+%inputs and output must contain digits only; no commas, spaces or any other characters are allowed. If any of
+%these assumptions are violated by the input, the function returns the number -1. Hint: you cannot use any of
+%the built-in numerical data types of MATLAB to do the calculation since the function needs to work for
+%numbers that are larger than what can be represented as a number in MATLAB.
+
+%problem huge add
+function summa = huge_add(a,b)
+    if ~ischar(a) || ~ischar(b) || sum(isstrprop(a,'digit')) ~= length(a) || ...
+            sum(isstrprop(b,'digit')) ~= length(b)
+        summa = -1;
+        return;
+    end
+    lng = max([length(a) length(b)]);
+    a = [a(end:-1:1) '0'+zeros(1,lng-length(a))];   % flip and pad with zeros if necessary
+    b = [b(end:-1:1) '0'+zeros(1,lng-length(b))];   % flip and pad with zeros if necessary
+    carry = 0;
+    for ii = 1:lng
+        c = carry + str2num(a(ii)) + str2num(b(ii)) % add the digits plus the caryy
+        carry = c > 9;                              % is there carry? (0 or 1)
+        summa(ii) = num2str(mod(c,10));             % put the character in the result
+    end
+    if carry
+        summa(end+1) = '1';                         % need a leading 1 if carry
+    end
+    summa = summa(end:-1:1);                        % flip it back
+end
+
+%Q4: 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any
+%remainder. Write a function called smallest_multiple that returns a uint64, the smallest positive
+%number that is evenly divisible by all of the numbers from 1 to n where n is a positive integer scalar and is
+%the only input argument of the function. If the result would be greater than what can be represented as a
+%uint64, the function returns 0. (Inspired by Project Euler.)
+
+%problem smallest multiple
+function mul = smallest_multiple(n)
+    facts = zeros(1,n);             % store the exponents of various factors
+    for ii = 2:n
+        f = factor(ii);             % get factors for current integer
+        for jj = 2:ii
+            k = sum(f == jj);       % what's the exponent of this factor?
+            if k > facts(jj)        % if it is greater than what we have so far
+                facts(jj) = k;      % update to this new value
+            end
+        end
+    end
+    % Compute the result with one command. 
+    % The 'native' option tells MATLAB to work in uint64
+    mul = prod(uint64((1:n).^facts),'native');   
+    if mul == intmax('uint64')
+       mul = uint64(0);
+    end
+end
+
+%%alternative solution to smallest multiple
+function r = smallest_multiple(k) 
+    r = uint64(1);
+    for n = 1:k
+        r = r * (n / gcd(r,n));   % Include factor not already present in r
+    end
+    if r == intmax('uint64')
+       r = uint64(0);
+    end
+end
+
+%Q5: Write a function called maxproduct that takes a matrix A and a positive integer scalar n as inputs and
+%computes the largest product of n adjacent elements in the same direction in A. That is, we are looking for
+%products of consecutive elements in the same row, column, diagonal or reverse diagonal. The function must
+%return an n-by-2 matrix containing the row and column indexes ordered first by row and then by column. If
+%no such product exists, the function returns the empty array. For example, valid outputs for a max product
+%of 3 neighbors in four different matrices might be [2 2; 2 3; 2 4] or [1 1; 2 1; 3 1] or [3 5; 4 4; 5 3] or [4 2; 5 3;
+%6 4]. If there are multiple products with the same maximum value, return the first one you find. (Inspired by
+%Project Euler.)
+
+%problem max product
+function B = maxproduct(A,n)
+
+% After checking that we do not have to return an empty array, we initialize a row vector
+% for remembering a product, home row and column, and one of four direction codes.
+% Home is a location in the matrix from which we can move in one of the directions
+% (1=East, 2=South, 3=SouthEast, 4=SouthWest). For each direction, home is systematically 
+% moved to everywhere it legally can be the start of n values. For each location, we 
+% potentially update the L vector with the largest product so far, the home location and
+% direction that corresponded to that product. After scanning in all four directions, we 
+% use L to reconstruct the required indices based on the last updated home and direction. 
+
+  [r,c] = size(A);
+  if n>r && n>c
+    B = [];                                    % cannot be solved
+    return
+  end
+
+  L = [-Inf,0,0,0];                            % [product, home-row, home-col, direction]
+      
+  for i=1:r                            
+    for j=1:c-n+1
+      L = check(A(i,j:j+n-1),[i,j,1],L);       % row, right case
+    end
+  end 
+  for i=1:r-n+1                        
+    for j=1:c
+      L = check(A(i:i+n-1,j),[i,j,2],L);       % column, down case
+    end
+  end
+  for i=1:r-n+1
+    for j=1:c-n+1
+      S=A(i:i+n-1,j:j+n-1);
+      L = check(diag(S),[i,j,3],L);            % diagonal, down case
+      L = check(diag(flip(S,2)),[i,j,4],L);    % reverse diagonal, down case
+    end
+  end
+  
+  i=L(2); j=L(3);                              % reconstruct coordinates
+  switch L(4)                                  
+    case 1, B = [ones(n,1)*i,(j:j+n-1)'];      
+    case 2, B = [(i:i+n-1)',ones(n,1)*j];
+    case 3, B = [(i:i+n-1)',(j:j+n-1)'];
+    case 4, B = [(i:i+n-1)',(j+n-1:-1:j)'];
+  end
+end
+
+function L = check(V,d,L)
+  p = prod(V);
+  if p>L(1)                                    % if new product larger than any previous
+    L = [p,d];                                 % then update product, home and direction 
+  end  
+end
+
+%Q6: If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4
+%= 19 letters used in total. Write a function called number2letters that returns the number of letters
+%needed to write down the number n in words. For example, 342 (three hundred forty two) contains 20
+%letters. Notice that we do not count spaces, nor do we use hyphens. The only input to the function is n, a
+%positive integer smaller than 1000, but you do not need to check this. (Inspired by Project Euler.)
+
+%problem number to letters
+function m = number2letters (n)
+
+% A is a 3x10 array of the letter counts of the words used to form various numbers, with
+% units in row 1, "teens" in row 2 and tens in row 3. For the supplied number, we first
+% determine the number of units, tens and hundreds. These three values determine the
+% words and thus the letter count to be returned.
+ 
+  A = [ 0 3 3 5 4 4 3 5 5 4; ...              % units
+        3 6 6 8 8 7 7 9 8 8; ...              % "teens"
+        0 0 6 6 5 5 5 7 6 6];                 % tens
+  h = fix(n/100); 
+  t = fix(rem(n,100)/10); 
+  u = rem(n,10);
+  if h>0,     m = A(1,h+1)+7;                % h 'hundred'
+  else        m = 0; 
+  end
+  switch t
+    case 0,   m = m+A(1,u+1);                % units only
+    case 1,   m = m+A(2,u+1);                % "teens" only
+    otherwise m = m+A(3,t+1)+A(1,u+1);       % tens and units
+  end 
+end
+
+%alternative solutio: This can be generalized to all numbers
 % function for number2letters: count number of letters to write a number name in words
 function out_letter = number2letters(num_input) 
    in_words = num2words(num_input);
@@ -153,6 +324,11 @@ function words = int2words_1to999(number)
     end
 end
 
+%Q7: Write a function called circular_primes that finds the number of circular prime numbers smaller
+%than n, where n is a positive integer scalar input argument. For example, the number, 197, is a circular
+%prime because all rotations of its digits: 197, 971, and 719, are themselves prime. For instance, there are
+%thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97. It is important to emphasize
+%that rotation means circular permutation not all possible permutations.
 
 %circular primes function
 function [ ncp ] = circular_primes( n )
@@ -228,3 +404,39 @@ function nd= ndigits(A)
 % find how many digits are in a number
     nd=floor(log10(abs(A)+1)) + 1;
 end
+
+%Q8: Write a function that is called like this [E,N] = cyclotron(V). All arguments are scalars. The input argument 
+%is the voltage applied to a cyclotron (figure), which is a device that accelerates subatomic
+%particles—in this case, positively charged isotopes of hydrogen, called “deuterons”—which spiral outward in a
+%clockwise direction. The cyclotron rapidly alternates the sign of the voltage difference V in units of volts between
+%two “D”-shaped vacuum chambers (blue outlines), which are placed within a strong uniform magnetic field (not shown 
+%but perpendicular to the page). The deuteron is accelerated only as it is leaving one “D” and entering the
+%other. While the deuteron is inside a given “D”, it moves at a constant speed, and the magnetic field causes it to move
+%on a semicircle. Each deuteron moves as follows (check the numbers in the figure): (1) It originates from a source (red
+%dot) located at a distance ss 0 to the left of the center of the cyclotron, is accelerated vertically into the upper “D” 
+%and then moves on a semi-circle of radius r 1 . (2) It leaves the upper “D” and is accelerated vertically downward into the
+%lower “D” where moves with a larger radius r 2 . (3) It leaves the lower “D” and is accelerated vertically into the upper
+%“D”, etc, moving with ever increasing radii rr nn until (N) it is accelerated for the final time as it leaves the
+%upper “D” and enters the lower “D”, follows a semicircle of radius r N , and emerges from the cyclotron at the
+%left. The formulas for the radii is given.
+
+%problem cyclotron
+function [E,n] = cyclotron (V)
+    m = 3.344e-27;           % mass of deuteron [kg]             
+    q = 1.603e-19;           % charge of deuteron [c]
+    B = 1.600;               % magnetic field strength [t]
+    z = m*V/(q*B^2);         % initial trajectory radius squared [m^2]
+    n = 0;                   % pass number
+    d = 2;                   % diameter/radius, sign alternates
+    r = sqrt(z);             % initial radius of curvature 
+    x = -r/2.0;              % initial x-coordinate
+
+    while x>=-0.5            % while deuteron has not emerged 
+        x = x+d*r;           % spiral to next x-coordinate ;
+        n = n+1;             % update pass number
+        d = -d;              % ... and direction
+        r = sqrt(r^2+2*z);   % calculate new radius in case needed
+    end
+    E = V*n*1e-6;            % determine energy  
+end
+
